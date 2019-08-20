4954c3604e87947fb4de84e00f83c9cfc6a5479f
arunkumarun <arunkumarun.r@gmail.com>
2019-08-20 11:38:32 +0930

    Updated with distanceTo method

--- src/com/ec/beans/City.java
+++ src/com/ec/beans/City.java
@@ -1,8 +1,8 @@
 package com.ec.beans;
 
 public class City {
-    int id;
-    Coor coor;
+    private int id;
+    private Coor coor;
 
     public int getId() {
         return id;
@@ -18,5 +18,17 @@ public class City {
 
     public void setCoor(Coor coor) {
         this.coor = coor;
+    }
+
+    public double distanceTo(City city){
+        int distX = Math.abs(getCoor().getX() - city.getCoor().getX());
+        int distY = Math.abs(getCoor().getY() - city.getCoor().getY());
+        double distance = Math.sqrt((distX*distX)+(distY*distY));
+        return distance;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" + id + " " + coor.getX() + " " + coor.getY() + '}';
     }
 }

517aace954917572eb81ec3a91e3d47e08a98063
MickeyZeng <373330860@qq.com>
2019-08-20 12:51:55 +0930

    Combine representation crossover

--- src/com/ec/Individual.java
+++ src/com/ec/Individual.java
@@ -1,98 +1,87 @@
 package com.ec;
 
+import com.ec.beans.City;
+
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Individual {
-    private static class City{
-        public String getName() {
-            return name;
-        }
+    private List<City> individual = new ArrayList<>(); //Holds list of city path.
+    private double fitness = 0;
+    private int distance = 0;
 
-        public City(String name, double latitude, double longitude) {
-            this.name = name;
-            this.latitude = latitude;
-            this.longitude = longitude;
-        }
-
-        String name;
-        double latitude;
-
-        public double getLongitude() {
-            return longitude;
-        }
-
-        public void setLongitude(double longitude) {
-            this.longitude = longitude;
-        }
-
-        double longitude;
-
-        public double getLatitude() {
-            return latitude;
-        }
-
-        public void setLatitude(double latitude) {
-            this.latitude = latitude;
+    // Constructor that generate individual with null
+    public Individual(){
+        for(int i=0; i<TSPProblem.getCitySize(); i++){
+            individual.add(null);
         }
     }
-    public static void main(String[] args){
-        List<City> cities= Arrays.asList(new City[]{
-                new City("a", 1, 2),
-                new City("b", 1, 2),
-                new City("c", 1, 2),
-                new City("d", 1, 2),
-                new City("e", 1, 2)
-        });
 
-        List<List<City>> permCities  = permute(cities);
+    public Individual(ArrayList individual){
+        this.individual = individual;
+    }
 
-        for(List<City> cityArr: permCities){
-            for(City city: cityArr){
-                System.out.print(" "+ city.name+",");
+    //Creates a random individual
+    public void generateIndividual(){
+        for(int i= 0; i<TSPProblem.getCitySize(); i++){
+            setCity(i,TSPProblem.getCity(i));
+        }
+        Collections.shuffle(individual);
+    }
+
+    //Gets a city from individual
+    public City getCity(int position){
+        return individual.get(position);
+    }
+
+    //Sets a city to an individual
+    public void setCity(int position, City city){
+        individual.set(position,city);
+        //Resetting since individual is altered.
+        fitness = 0;
+        distance = 0;
+    }
+
+    //Gets individual fitness
+    public double getFitness(){
+        if(fitness == 0){
+            fitness = 1/getDistance();
+        }
+        return fitness;
+    }
+
+    //Gets the total distance of individual's all city
+    public double getDistance(){
+        if(distance == 0){
+            int individualDist = 0;
+            for(int individualIndex=0; individualIndex < individualSize(); individualIndex++){
+                City fromCity = getCity(individualIndex);
+                City destCity;
+                //Check we're not on our individual's last city, if we are set our individual's final
+                // destination city to our starting city
+                if(individualIndex+1 < individualSize()){
+                    destCity = getCity(individualIndex+1);
+                }
+                else{
+                    destCity = getCity(0);
+                }
+
+                individualDist += fromCity.distanceTo(destCity);
             }
-            System.out.println();
+            distance = individualDist;
         }
+        return distance;
     }
 
-    public double calculateDistance(City a, City b){
-        double x1 = a.getLatitude(),
-                x2=a.getLongitude(),
-                y1=b.getLatitude(),
-                y2=b.getLongitude();
-        double dis;
-        dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-        return dis;
+    // returns individual size
+    public int individualSize(){
+        return individual.size();
     }
 
-    private static void swap(List<City> nums, int i, int j){
-        City temp = nums.get(i);
-        nums.set(i,nums.get(j));
-        nums.set(j,temp);
-    }
-
-    public static List<List<City>> permute(List<City> nums) {
-        List<List<City>> result = new ArrayList<>();
-        helper(0, nums, result);
-        return result;
-    }
-
-    private static void helper(int start, List<City> nums, List<List<City>> result){
-        if(start==nums.size()-1){
-            ArrayList<City> list = new ArrayList<City>();
-            for(City num: nums){
-                list.add(num);
-            }
-            result.add(list);
-            return;
-        }
-
-        for(int i=start; i<nums.size(); i++){
-            swap(nums, i, start);
-            helper(start+1, nums, result);
-            swap(nums, i, start);
-        }
+    //check individual contains city
+    public boolean containsCity(City city){
+        return individual.contains(city);
     }
 
 }

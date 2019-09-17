8933c6ffb07884b1d13a920e136f3201a18566ab
Nipuna H Herath <hash.crackhead@gmail.com>
2019-09-17 16:33:01 +0930

    add coords generator

--- src/com/ec/SampleMain.java
+++ src/com/ec/SampleMain.java
@@ -1,26 +1,35 @@
 package com.ec;
 
+import com.ec.beans.DataInstance;
+
+import java.util.ArrayList;
+
 public class SampleMain {
 
     public static void main(String[] args) throws Exception{
-        TSPProblem tsp = new TSPProblem("data/eil51.tsp.txt");
-        System.out.println(tsp.getCities());
-
-        EvolutionAlgorithm ea = new EvolutionAlgorithm();
-        ea.setPopulationSize(50);
-        ea.setGenerationSize(2000);
-        ea.setSelection(EvolutionAlgorithm.TOURNAMENT_SELECTION);
-        ea.setTournamentSize(5);
-        ea.setCrossover(EvolutionAlgorithm.CYCLE_CROSSOVER);
-        ea.setElitism(1);
-        ea.setMutation(EvolutionAlgorithm.INVERSION_MUTATION);
-
-        ea.run(tsp.getCities());
-        System.out.println("EA Worked");
-
-//        InverOver io = new InverOver();
-//        io.algorithm();
-        System.out.println("IO Works");
+//        TSPProblem tsp = new TSPProblem("data/eil51.tsp.txt");
+//        System.out.println(tsp.getCities());
+//
+//        EvolutionAlgorithm ea = new EvolutionAlgorithm();
+//        ea.setPopulationSize(50);
+//        ea.setGenerationSize(2000);
+//        ea.setSelection(EvolutionAlgorithm.TOURNAMENT_SELECTION);
+//        ea.setTournamentSize(5);
+//        ea.setCrossover(EvolutionAlgorithm.CYCLE_CROSSOVER);
+//        ea.setElitism(1);
+//        ea.setMutation(EvolutionAlgorithm.INVERSION_MUTATION);
+//
+//        ea.run(tsp.getCities());
+//        System.out.println("EA Worked");
+//
+////        InverOver io = new InverOver();
+////        io.algorithm();
+//        System.out.println("IO Works");
+        DataInstance ind = new DataInstance();
+        ArrayList<String> cities = ind.randXYPoint(10,0,50);
+        for(String s: cities){
+            System.out.println(s);
+        }
     }
 
 }
--- src/com/ec/beans/DataInstance.java
+++ src/com/ec/beans/DataInstance.java
@@ -1,9 +1,26 @@
 package com.ec.beans;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class DataInstance {
-    List<List<City> cities> instance = new ArrayList<ArrayList<City>>();
+    public ArrayList randXYPoint(int numberOfCities, int min, int max) throws IOException {
+        String fileName = "src/com/ec/"+"fileName" + numberOfCities+".tsp.txt";
+        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+        ArrayList<String> cities = new ArrayList<String>();
+        for (int i = 1; i <= numberOfCities; i++) {
+            // int yValue = min + rand.nextInt(50);
+            int xValue = (int) ((Integer) min + Math.random() * (max - min));
+            // generates y values
+            int yValue = (int) (min + Math.random() * (max - min));
+            String city = String.valueOf(i) + " " + String.valueOf(xValue) + " " + String.valueOf(yValue) + "\n";
+            writer.write(city);
+            cities.add(city);
+        }
+        writer.close();
+        return cities;
+    }
 
 }
